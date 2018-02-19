c4e25a7d7ab598fbc4dc5ead3b5e94d15b9df859
keertikamath <36561920+keertikamath@users.noreply.github.com>
2018-02-19 15:52:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='New value';
+
+var img=document.getElemeentById('madi');
+img.onClick=function(){
+    img.style.marginLeft='100px';
+}
