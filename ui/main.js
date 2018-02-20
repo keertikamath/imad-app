9ed49be7372a400c9632903c7b5d55ede33c34c1
keertikamath <36561920+keertikamath@users.noreply.github.com>
2018-02-20 15:35:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,12 +24,15 @@ button.onclick= function()
     
     //Capture the response and store it in a variable
     request.onreadystatechange= function(){
-        if(request.readyState === XMLHttpRequest.DONE)
+        if(request.readyState === XMLHttpRequest.DONE){
+          if(request.status === 200)    
+        
         {
             var counter= request.responseText;
             var span= document.getElementById('count');
             span.innerHTML=counter.toString();
         }
+     }
     };
     //make the request
     request.open('GET','http://keertikamath.imad.hasura-app.io',true);
